28388caa7029e4c6e659dff23f7bb619a1fc5f66
Rowan Polster <rowandownriver94@gmail.com>
2021-06-09 20:24:39 -0500

    maybe this time is different

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // TODO: add code here
+let apiUrl = "https://handlers.education.launchcode.org/static/astronauts.json"
+
+window.addEventListener("load", function() {
+  fetch(apiUrl)
+  .then(function(response) {
+  response.json()
+  .then(function(json) {
+    console.log(json);
+    let div = document.getElementById("container");
+    for (i = 0; i < json.length; i++) {
+      div.innerHTML += `
+        <div class="astronaut">
+          <div class="bio">
+            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+            <ul>
+              <li>Hours in space: ${json[i].hoursInSpace}</li>
+              <li>Active: ${json[i].active}</li>
+              <li>Skills: ${json[i].skills.join(", ")}</li>
+            </ul>
+          </div>
+          <img class="avatar" src="${json[i].picture}">
+        </div>
+      `;
+        }
+});
+});
+});
